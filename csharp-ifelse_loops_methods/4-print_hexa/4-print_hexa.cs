810c4532ce06196ad7df19d4f1abeef8e2e66ea9
Thomas <thomasviard@orange.fr>
2024-12-02 21:52:32 +0100

    i knew it, i lost the ability to code

--- csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        for (int i = 0; int i <= 98; i++)
+        for (int i = 0; i <= 98; i++)
         {
             Console.Write($"{i} = 0x{i:x}\n");
         }
