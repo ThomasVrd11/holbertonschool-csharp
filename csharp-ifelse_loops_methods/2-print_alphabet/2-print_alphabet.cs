f4fec9cc52c843a69b5ad3f19b66d19d51308bf1
Thomas <thomasviard@orange.fr>
2024-12-02 21:34:30 +0100

    new tasks, correct dotnet versions

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char alphabet = 'a'; alphabet = 'z'; alphabet++)
+        Console.Write(alphabet);
+    }
+}
