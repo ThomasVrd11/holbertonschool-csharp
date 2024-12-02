0297d4c9549c9c06968d5a2f38470575bd5a02fc
Thomas <thomasviard@orange.fr>
2024-12-02 21:50:09 +0100

    task 4, bad feeling about this

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; int i <= 98; i++)
+        {
+            Console.Write($"{i} = 0x{i:x}\n");
+        }
+    }
+}
