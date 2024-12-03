67a80d5619927f3ec395ee9926f979f44abdebee
Thomas <thomasviard@orange.fr>
2024-12-03 11:41:20 +0100

    typo again, cant write correctly static

--- csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -2,13 +2,14 @@
 
 class Line
 {
-    public status void PrintLine(int length)
+    public static void PrintLine(int length)
     {
         if (length > 0)
         {
             for (int i = 0; i <= length; i++)
                 Console.Write("_");
         }
+
         Console.Write("\n");
     }
 }
