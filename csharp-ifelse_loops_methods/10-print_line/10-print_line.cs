f2d07f4cf14071d9d2ae140e478a1cf3b5e0ffaf
Thomas <thomasviard@orange.fr>
2024-12-03 11:37:27 +0100

    not sure with indentation but task 9, we ll see

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Line
+{
+    public sttaus void PrintLine(int length)
+    {
+        if (length > 0)
+        {
+            for (int i = 0; i <= length; i++)
+            Console.Write("_");
+        }
+        Console.Write("\n");
+    }
+}
