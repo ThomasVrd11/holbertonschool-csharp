b15fbb549dfd04aace2e9f66ed4ff1924bbdf075
Thomas <thomasviard@orange.fr>
2024-12-03 11:15:12 +0100

    task 8 last digit

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int isLD = number % 10;
+        if (isLD < 0)
+            isLD *= -1;
+        Console.Write(isLD);
+
+        return isLD;
+    }
+}
