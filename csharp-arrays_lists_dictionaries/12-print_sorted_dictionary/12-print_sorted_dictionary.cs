aa89fbfebe5b1459cefc520ec25dcb68fe728301
Thomas <thomasviard@orange.fr>
2024-12-03 22:24:26 +0100

    these ones were cool actually, pretty easy (copium)

---
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<string> keys = new List<string>(myDict.keys)
+        keys.Sort();
+
+        foreach (string ikey in keys)
+        {
+            Console.WriteLine("{0}: {1}", ikey, myDict[key]);
+        }
+    }
+}
