83443f69f9e959a0b9c54689cd077d84cb89690d
Thomas <thomasviard@orange.fr>
2024-12-03 13:44:49 +0100

    again

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -1,12 +1,11 @@
-﻿using System;
-
+﻿
 class Program
 {
 	static void Main(string[] args)
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-		Console.WriteLine(string.Format("Percent: {0:0.00%}", percent));
-		Console.WriteLine("Currency: " + currency.ToString("C", System.Globalization.CultureInfo.CreateSpecificCulture("en-US.utf-8")));
+        Console.WriteLine("Percent: {0:P2}", percent);
+        Console.WriteLine($"Currency: {currency.ToString("C2", System.Globalization.CultureInfo.GetCultureInfo("en-US"))}");
 	}
 }
