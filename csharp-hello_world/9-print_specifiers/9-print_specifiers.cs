e4c8549c6267318b0b2464191e09eba363248907
Thomas <thomasviard@orange.fr>
2024-12-03 12:13:41 +0100

    trying globalization

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -1,12 +1,12 @@
-﻿using System;
+﻿using System; using System.Globalization;
 
 class Program
 {
-	static void Main(string[] args)
-	{
-		double percent = .7553;
-		double currency = 98765.4321;
-        Console.WriteLine("Percent: {0:0.00%}", percent);
-        Console.WriteLine("Currency: {0:C2}", currency);
-	}
+    static void Main(string[] args)
+    {
+        double percent = .7553;
+        double currency = 98765.4321;
+        Console.WriteLine("Percent: {0:P2}", percent);
+        Console.WriteLine("Currency: {0:C}", currency);
+    }
 }
