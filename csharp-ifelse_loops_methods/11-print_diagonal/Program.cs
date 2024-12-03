8bc6f40cc24b5d08b7c8b68550dbfe8246e8174c
Thomas <thomasviard@orange.fr>
2024-12-03 11:51:04 +0100

    first try diagonal

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length < 0)
+            Console.WriteLine();
+        else
+        {
+            string empty = "";
+            
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine(empty + "\\"):
+                empty += " ";
+            }
+            Console.WriteLine();
+        }
+    }
+}
