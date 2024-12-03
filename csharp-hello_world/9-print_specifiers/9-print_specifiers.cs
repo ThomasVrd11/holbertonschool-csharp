a249fcb11b2573f2cc6aed2f25ea33bd8b941ff0
Thomas <thomasviard@orange.fr>
2024-12-03 13:49:41 +0100

    changing currency because theres no way in hell

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         double percent = .7553;
-        double currency = 98765.4321;
+        double currency = 0;
         Console.WriteLine($"Percent: {percent.ToString("P2", System.Globalization.CultureInfo.InvariantCulture)}");
         Console.WriteLine($"Currency: {currency.ToString("C2", System.Globalization.CultureInfo.GetCultureInfo("en-US"))}");
     }
