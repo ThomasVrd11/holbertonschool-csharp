92f044e4bf820edd383a54aff5e73818dcc5960e
Thomas <thomasviard@orange.fr>
2024-12-03 11:01:46 +0100

    printing combinations of digits

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i <= 99; i++)
+        {
+            int n = i % 10;
+            int m = i / 10;
+
+            if (n != m && m < n)
+                {
+                    if (i != 89)
+                        Console.Write("{0:D2}, ", i);
+                    else
+                        Console.WriteLine("{0:D2}", i);
+                }
+        }
+    }
+}
