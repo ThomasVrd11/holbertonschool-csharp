379ec174bef6c64774122b37c51be1fdd415f3ba
Thomas <thomasviard@orange.fr>
2024-12-04 11:43:00 +0100

    i had my version but too long, changed it

--- csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList == null || myList.Count == 0)
+            return "None";
+
+        KeyValuePair<string, int> best = myList.OrderByDescending(item => item.Value).First();
+        return best.Key;
+    }
+}
