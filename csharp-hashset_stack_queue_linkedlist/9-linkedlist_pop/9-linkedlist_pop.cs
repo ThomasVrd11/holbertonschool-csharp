15f88c4dfbcdeddf5ea7ef92d5b327db65ed21e0
Thomas <thomasviard@orange.fr>
2024-12-09 14:46:37 +0100

    goodbye headnode

---
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> mylist)
+    {
+        if (myLList.Count == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            int headNode = myLList.First.Value;
+            myLList.RemoveFirst();
+            return headNode;
+        }
+    }
+}
