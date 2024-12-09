b52c6fd6c6d157d9f7d054756d9e6d84e11390f5
Thomas <thomasviard@orange.fr>
2024-12-09 15:05:18 +0100

    task 10

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int currentNode = 0;
+
+        foreach (int node in myLList)
+        {
+            if (currentNode == n)
+            {
+                return node;
+            }
+            currentNode++;
+        }
+        return 0;
+    }
+}
