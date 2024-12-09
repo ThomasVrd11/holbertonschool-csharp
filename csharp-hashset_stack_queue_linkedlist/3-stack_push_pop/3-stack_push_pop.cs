e8cd8a8bb1e8ce51a88373ffd07f904130e1ffbb
Thomas <thomasviard@orange.fr>
2024-12-09 11:53:33 +0100

    first test of task 3, a lot of condtions huh

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        System.Console.WriteLine("Number of items: " + aStack.Count);
+
+        if (aStack.Count == 0)
+        {
+            System.Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            System.Console.WriteLine("Top item: " + aStack.Peek());
+        }
+
+        if (aStack.Contains(search))
+        {
+            System.Console.WriteLine("Stack contains " + search + ": True");
+        }
+        else 
+        {
+            System.Console.WriteLine("Stack contains " + search + ": False");
+        }
+
+        if (aStack.Contains(search))
+        {
+            while (aStack.Contains(search))
+            {
+                aStack.Pop();
+            }
+        }
+
+        aStack.Push(newItem);
+
+        return aStack;
+    }
+}
