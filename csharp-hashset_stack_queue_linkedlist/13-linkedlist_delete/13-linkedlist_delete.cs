f069c127c3c8f2ce76b84cc6b20dbaa285d3c83c
Thomas <thomasviard@orange.fr>
2024-12-09 15:55:37 +0100

    deleting a node holbertonschoolbyactual

--- csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> holbertonschoolByActual = myLList.First;
+        int i = 0;
+
+        while (holbertonschoolByActual != null)
+        {
+            if (i == index)
+            {
+            myLList.Remove(holbertonschoolByActual);
+            break;
+            }
+        }
+        i++;
+        holbertonschoolByActual = holbertonschoolByActual.Next;
+    }
+}
