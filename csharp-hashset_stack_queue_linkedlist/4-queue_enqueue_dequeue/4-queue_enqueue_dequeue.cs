689ea6bc65c0a0631f25e0d7fd2a514022d7f843
Thomas <thomasviard@orange.fr>
2024-12-09 12:02:44 +0100

    same task as task 3, very fast and efficient

---
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        System.Console.WriteLine("Number of items: " + aQueue.Count);
+
+        if (aQueue.Count == 0)
+        {
+            System.Console.WriteLine("Queue is empty");
+        }
+        else
+        {
+            System.Console.WriteLine("Top item: " + aQueue.Peek());
+        }
+
+        aQueue.Enqueue(newItem);
+
+        if (aQueue.Contains(search))
+        {
+            System.Console.WriteLine("Queue contains \"" + search + "\": True");
+        }
+        else 
+        {
+            System.Console.WriteLine("Queue contains \"" + search + "\": False");
+        }
+
+        if (aQueue.Contains(search))
+        {
+            while (aQueue.Contains(search))
+            {
+                aQueue.Dequeue();
+            }
+        }
+
+        return aQueue;
+    }
+}
