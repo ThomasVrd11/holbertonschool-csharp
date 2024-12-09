55a4031493ca213f167799ea90bd7f3befb2c41d
Thomas <thomasviard@orange.fr>
2024-12-09 11:29:33 +0100

    task 2, check for different elements in 2 lists

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> different = new List<int>();
+
+        // elements from list 2 that are not in list 1
+        foreach (int element in list1)
+        {
+            if (!list2.Contains(element))
+            {
+                different.Add(element);
+            }
+        }
+        // same but list 1 and not list 2
+        foreach (int element in list2)
+        {
+            if (!list1.Contains(element))
+            {
+                different.Add(element);
+            }
+        }
+
+        different.Sort();
+        
+        return different;
+    }
+}
