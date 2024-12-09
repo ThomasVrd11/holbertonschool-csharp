9fcd1b64b063243a7d57e060ed67fd7433eac67c
Thomas <thomasviard@orange.fr>
2024-12-09 15:40:52 +0100

    it should work but im kinda lost

---
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> newNode = new LinkedListNode<int>(n);
+        LinkedListNode<int> currentNode = myLList.First;
+
+        if (currentNode == null || currentNode.Value > n)
+        {
+            myLList.AddFirst(newNode);
+            return newNode;
+        }
+
+        while (currentNode.Next != null && currentNode.Next.Value < n)
+        {
+            currentNode = currentNode.Next;
+        }
+
+        myLList.AddAfter(currentNode, newNode);
+        return newNode;
+    }
+}
