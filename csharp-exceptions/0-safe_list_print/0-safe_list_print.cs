3e1996f363f8b73630d1659246289e021c693eab
Thomas <thomasviard@orange.fr>
2025-01-03 10:48:44 +0100

    all project 1

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int numberOfPrint = 0;
+        while (numberOfPrint < n)
+        {
+            try
+            {
+                Console.WriteLine(myList[numberOfPrint]);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                break;
+            }
+            numberOfPrint++;
+        }
+        return numberOfPrint;
+    }
+}
