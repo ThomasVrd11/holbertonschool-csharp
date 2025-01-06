86c2781b9bf36aeb70ffa4c1f172456c2bf54195
Thomas <thomasviard@orange.fr>
2025-01-06 11:37:30 +0100

    task 1

--- csharp-classes/1-enemy/1-enemy.cs
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> blueprint for zombie. </summary>
+    public class Zombie 
+    {
+         /// <summary> Health </summary>
+        public int health;
+
+        /// <summary> new instance of a zombie </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
