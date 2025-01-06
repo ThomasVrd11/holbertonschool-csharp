76c8970173f6b4286fb6beefae08200b18941004
Thomas <thomasviard@orange.fr>
2025-01-06 13:55:42 +0100

    task 2

--- csharp-classes/2-enemy/2-enemy.cs
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> blueprint for zombie. </summary>
+    public class Zombie 
+    {
+         /// <summary> Health </summary>
+        public int health;
+
+        /// <summary> new instance of a zombie </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+    }
+}
