ca12c6a7c492b02f17587ee18480a0fb28b868a5
Thomas <thomasviard@orange.fr>
2025-01-06 14:31:09 +0100

    allez ça part

--- csharp-classes/4-enemy/4-enemy.cs
+++ csharp-classes/4-enemy/4-enemy.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> Class of a zombie </summary>
+    public class Zombie
+    {
+        private int health;
+        private string name = "(no name)";
+
+        /// <summary> Constructor </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary> Constructor for a new instance of the zombie class with a given health value. </summary>
+        public Zombie(int value)
+        {
+            if (value < 0) throw new ArgumentException("Health must be greater than or equal to 0");
+            health = value;
+        }
+
+        /// <summary> Gets sets zombie name </summary>
+        public string Name
+        {
+            get => name;
+            set => name = givenName;
+        }
+
+        /// <summary> Method that returns the value of Zombie health </summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
