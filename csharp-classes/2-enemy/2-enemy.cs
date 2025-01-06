911ae197665193323ea30be3a3975a8f58b6a518
Thomas <thomasviard@orange.fr>
2025-01-06 13:58:52 +0100

    again

--- csharp-classes/2-enemy/2-enemy.cs
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -2,24 +2,22 @@
 
 namespace Enemies
 {
-    /// <summary> blueprint for zombie. </summary>
-    public class Zombie 
+    /// <summary> Class of a zombie </summary>
+    public class Zombie
     {
-         /// <summary> Health </summary>
+        /// <summary> Health </summary>
         public int health;
 
-        /// <summary> new instance of a zombie </summary>
+        /// <summary> Constructor </summary>
         public Zombie()
         {
             health = 0;
         }
 
+        /// <summary> Constructor for a new instance of the zombie class with a given health value. </summary>
         public Zombie(int value)
         {
-            if (value < 0)
-            {
-                throw new ArgumentException("Health must be greater than or equal to 0");
-            }
+            if (value < 0) throw new ArgumentException("Health must be greater than or equal to 0");
             health = value;
         }
     }
