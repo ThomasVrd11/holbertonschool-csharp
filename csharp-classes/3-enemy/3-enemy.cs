3290128465319690026f35530992849dc7dfc7b3
Thomas <thomasviard@orange.fr>
2025-01-06 14:02:10 +0100

    return health method

--- csharp-classes/3-enemy/3-enemy.cs
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> Class of a zombie </summary>
+    public class Zombie
+    {
+        /// <summary> Health </summary>
+        public int health;
+
+        /// <summary> Constructor </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary> Constructor for a new instance of the zombie class with a given health value. </summary>
+        public Zombie(int value)
+        {
+            if (value < 0) throw new ArgumentException("Health must be greater than or equal to 0");
+            health = value;
+        }
+
+        /// <summary> Method that returns the value of Zombie health </summary>
+        
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
