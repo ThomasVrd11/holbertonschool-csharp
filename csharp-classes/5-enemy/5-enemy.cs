fec3369fb787e002ef869d0ca6642b629fb3992b
Thomas <thomasviard@orange.fr>
2025-01-06 14:37:19 +0100

    printing his attribute

--- csharp-classes/5-enemy/5-enemy.cs
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> Class of a zombie </summary>
+    public class Zombie
+    {
+        private int health;
+        private string name = "(No name)";
+
+        /// <summary> Constructor </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary> Constructor for a new instance of the zombie class with a given health value. </summary>
+        public Zombie(int value)
+        {
+            if (value < 0) throw new ArgumentException("Health must be greater than or equal to 0");
+            health = value;
+        }
+
+        /// <summary> Gets sets zombie name </summary>
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        /// <summary> Method that returns the value of Zombie health </summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        /// <summary> Override that prints the zombie attribute to stdout </summary>
+        public .toString()
+        {
+            return $"Zombie Name: {name} / Total Health: {health}";
+        }
+    }
+}
